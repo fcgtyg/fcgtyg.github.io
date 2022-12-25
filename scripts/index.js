e710d5003f1e1d415e8e76663ef40725c6cafc36
Fatih Çağatay Gülmez <Fatih.gulmez@y42.com>
2022-12-26 01:26:46 +0300

    career and projects data

--- scripts/index.js
+++ scripts/index.js
@@ -7,6 +7,10 @@ $( document ).ready(function(){
 
     generate_html("db/certificate.json", generate_certificate_html, "certificate-body")
 
+    generate_html("db/experience.json", generate_career_html, "career-body")
+
+    generate_html("db/project.json", generate_project_html, "projects-body")
+
 })
 
 function generate_html(file_path, func, element_id){
@@ -86,9 +90,96 @@ function generate_certificate_html(data){
     `
 }
 
-function generate_skill_badge(data){
-    bg = "bg-success"
+function generate_career_html(data){
+    start_date = data.start_date
+    start_date_split = start_date.split(" ")
+    end_date = "Currently Working"
+    if(data.end_date){
+        end_date = "Until " + data.end_date
+    }
+    reference_html = ""
+    if(data.references)
+        reference_html = generate_career_reference_html(data.references)
+    skill_html = generate_skill_badge(data.tech_stack)
     return `
-        <span class="badge ${bg}">${data.title}</span>
+        <div class="timeline-row">
+            <div class="timeline-time">${start_date_split[0]}<small>${start_date_split[1]}</small></div>
+            <div class="timeline-content">
+                <i class="icon-attachment"></i>
+                <small>${end_date}</small>
+                <h4>${data.title}</h4>
+                <h6>${data.corporation}</h6>
+                <hr class="my-1" />
+                <p>
+                    ${data.description}
+                </p>
+                <p>
+                <span>References</span>
+                    ${reference_html}
+                </p>
+                <div class="thumbs">
+                <span class="badge bg-dark mx-1">${data.location}</span>
+                <span class="badge bg-dark mx-1">${data.place}</span>
+                <span class="badge bg-dark mx-1">${data.type}</span>
+                </div>
+                <div>
+                    ${skill_html}
+                </div>
+            </div>
+        </div>
+    `
+}
+
+function generate_career_reference_html(data_list){
+    html = ""
+    for (let reference of data_list){
+        html += `
+            <a class="text-reset" href="${reference.profile}">${reference.name}</a>
+        `
+    }
+
+    return html
+}
+
+function generate_skill_badge(data_list){
+    html = ""
+    for(let data of data_list){
+        html += `
+            <span class="badge bg-success">${data}</span>
+        `
+    }
+    return html
+}
+
+
+function generate_project_html(data){
+    
+    if(data.end_date){
+        end_date = data.end_date
+    }else{
+        end_date = "InProgress Now"
+    }
+
+    end_date_split = end_date.split(" ")
+
+    skill_html = generate_skill_badge(data.tech_stack)
+    return `
+        <div class="timeline-row">
+        <div class="timeline-time">${end_date_split[1]}<small>${end_date_split[0]}</small></div>
+        <div class="timeline-content">
+            <i class="icon-code"></i>
+            <small>Started at ${data.start_date}</small>
+            <h4>${data.title}</h4>
+            <p>
+                ${data.description}
+            </p>
+            <div class="thumbs">
+            <span class="badge bg-dark mx-1">${data.corporation}</span>
+            </div>
+            <div>
+                ${skill_html}
+            </div>
+        </div>
+        </div>
     `
 }
